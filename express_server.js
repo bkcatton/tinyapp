a0326df579323c664653c0cafe7dbe725659c945
bkcatton <ben.k.catton@gmail.com>
2022-01-24 16:38:00 -0800

    Allowed the shortend url to resolve to real URL.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+const templateVars = { urls: urlDatabase };
+
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -12,15 +18,15 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
 
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
-
-  const templateVars = { urls: urlDatabase };
 
   res.render("./pages/urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("./pages/urls_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
