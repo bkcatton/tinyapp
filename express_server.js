47e84b9fce9c463ca01dc950f1c31d33762bf151
bkcatton <ben.k.catton@gmail.com>
2022-01-24 18:30:22 -0800

    Added a new URL page and a function that will gen a random string 6 characters long.

--- express_server.js
+++ express_server.js
@@ -6,10 +6,35 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 const templateVars = { urls: urlDatabase };
 
-//set the view engine to ejs
-app.set('view engine', 'ejs');
+function generateRandomString() {
+  let lettersAndNumbers = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
+    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
+    '2', '3', '4', '5', '6', '7', '8', '9'
+  ];
+let randString = [];
+for (let i = 0; i < 6; i++) {
+  let randInt = Math.floor(1+Math.random()*61)
+  randString.push(lettersAndNumbers[randInt]);
+}
+return randString.join("");
+}
+
+console.log(generateRandomString());
+
+
+
+//set the view engine to ejs app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   //.render('./pages/urls_index');
@@ -17,14 +42,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-
-
   res.render("./pages/urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("./pages/urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("./pages/urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
